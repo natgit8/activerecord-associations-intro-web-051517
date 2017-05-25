f07629bc0fc4300e50a3a84aabf44acb2dce4c1c
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-05-25 11:32:03 -0400

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,10 @@
 class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+    end
+  end
 end
+#
+# Artists have many songs and a song belongs to an artist.
+# Artists have many genres through songs.
--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,2 +1,10 @@
 class CreateGenres < ActiveRecord::Migration
+  def change
+   create_table :genres do |t|
+     t.string :name
+   end
+ end
 end
+
+# A genre has many songs.
+# A genre has many artists through songs.
